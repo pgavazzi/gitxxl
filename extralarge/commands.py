4b25763879ac6e8147967a149278168aa8e900e6
Paul Gavazzi <pgavazzi@nuvuja-2.local>
2014-11-22 14:11:51 -0500

    support subcommands

--- extralarge/commands.py
+++ extralarge/commands.py
@@ -1,3 +1,16 @@
 from i18n import _
 from argparse import ArgumentParser
 
+parser = ArgumentParser()
+parser.add_argument('-v','--version', help='show current version', action='store_true', default=False, required=False)
+subparsers = parser.add_subparsers(title='The most commonly used gitxxl commands are',
+								   metavar='<command>',
+								   help='command help')
+
+def init(req):
+	print "running init with req:", req
+
+parser_init = subparsers.add_parser('init', help='create an empty gitxxl repository or reinitialize an existing one')
+parser_init.add_argument('--convert', help='will convert an existing git repository to use xxl', action='store_true', default=False)
+parser_init.add_argument('path', help='path where the git repository is located and gitxxl repository will be initialized', default='.')
+parser_init.set_defaults(func=init)
--- extralarge/dispatch.py
+++ extralarge/dispatch.py
@@ -1,16 +1,13 @@
 from i18n import _
 import os, sys, atexit, signal, pdb, socket, errno, shlex, time, traceback, re
-#import util, commands, hg, fancyopts, extensions, hook, error
 import error
-#import cmdutil, encoding
-#import ui as uimod
+from commands import parser
 import git
 
 class request(object):
-    def __init__(self, args, ui=None, repo=None, fin=None, fout=None,
+    def __init__(self, repo=None, fin=None, fout=None,
                  ferr=None):
-        self.args = args
-        self.ui = ui
+        self.args = parser.parse_args()
         self.repo = repo
 
         # input/output/error streams
@@ -20,52 +17,19 @@ class request(object):
 
 def run():
     "run the command in sys.argv"
-    sys.exit((dispatch(request(sys.argv[1:])) or 0) & 255)
+    sys.exit((dispatch(request()) or 0) & 255)
 
 def dispatch(req):
     "run the command specified in req.args"
     if req.ferr:
         ferr = req.ferr
-    elif req.ui:
-        ferr = req.ui.ferr
     else:
         ferr = sys.stderr
 
-    try:
-        if not req.ui:
-            req.ui = uimod.ui()
-        if '--traceback' in req.args:
-            req.ui.setconfig('ui', 'traceback', 'on', '--traceback')
-
-        # set ui streams from the request
-        if req.fin:
-            req.ui.fin = req.fin
-        if req.fout:
-            req.ui.fout = req.fout
-        if req.ferr:
-            req.ui.ferr = req.ferr
-    except error.Abort, inst:
-        ferr.write(_("abort: %s\n") % inst)
-        if inst.hint:
-            ferr.write(_("(%s)\n") % inst.hint)
-        return -1
-    except error.ParseError, inst:
-        if len(inst.args) > 1:
-            ferr.write(_("hg: parse error at %s: %s\n") %
-                             (inst.args[1], inst.args[0]))
-            if (inst.args[0][0] == ' '):
-                ferr.write(_("unexpected leading whitespace\n"))
-        else:
-            ferr.write(_("hg: parse error: %s\n") % inst.args[0])
-        return -1
-
-    msg = ' '.join(' ' in a and repr(a) or a for a in req.args)
     starttime = time.time()
     ret = None
     try:
-        ret = _runcatch(req)
+        ret = req.args.func(req)
         return ret
     finally:
         duration = time.time() - starttime
-        req.ui.log("commandfinish", "%s exited %s after %0.2f seconds\n",
-                   msg, ret or 0, duration)
